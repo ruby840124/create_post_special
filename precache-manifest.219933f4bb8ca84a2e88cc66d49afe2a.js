d273a6994500034fa8d93d3a2588247bbcdd0086
ruby840124 <ruby840124@gmail.com>
2019-09-26 11:32:31 +0800

    Updates

---
+++ precache-manifest.219933f4bb8ca84a2e88cc66d49afe2a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38af7cb0131e6222552c95d81ad84272",
+    "url": "/create_post_special/index.html"
+  },
+  {
+    "revision": "35c3a234ac3f8be0aa0f",
+    "url": "/create_post_special/static/css/main.91b6c6b2.chunk.css"
+  },
+  {
+    "revision": "5b22ccf19ac3197679fb",
+    "url": "/create_post_special/static/js/2.b199ccf9.chunk.js"
+  },
+  {
+    "revision": "35c3a234ac3f8be0aa0f",
+    "url": "/create_post_special/static/js/main.a10ad3c4.chunk.js"
+  },
+  {
+    "revision": "5a3bba2886891528716e",
+    "url": "/create_post_special/static/js/runtime-main.c1ef0c01.js"
+  }
+]);
